3746e6b78336b9a9b3880dfff7c9de2fca38ec56
wettenmel <fabianmmelcher@gmail.com>
2025-07-22 20:49:22 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,3 +13,39 @@ def generate_story_with_images(data):
     # Dummy: Ersetze **ww**
     story_text = story_text.replace("**ww**", "[kreativ & mutig]")
     return story_text
+
+def extract_fields(data):
+    answers = data["form_response"]["answers"]
+    result = {}
+
+    field_map = {
+        "wtxhy97iY1LM": "name",
+        "vmzqqlkaZDRf": "alter",
+        "BwpW0kZdU2Do": "geschlecht",
+        "Pp9LylgrsxBF": "haarfarbe",
+        "DkeaoqEf2dxM": "frisur",
+        "kEOj5ikyylNE": "lieblingsfarben",
+        "HWMdGHmJdXdf": "thema",
+        "396Z2bchXQVR": "emotion",
+        "VCJ3yAEtp2on": "superkraft",
+        "sfxeeKJYAmeu": "tier",
+        "95Sdziapax8V": "widmung"
+    }
+
+    for answer in answers:
+        field_id = answer["field"]["id"]
+        key = field_map.get(field_id)
+
+        if not key:
+            continue
+
+        if answer["type"] == "text":
+            result[key] = answer["text"]
+        elif answer["type"] == "choice":
+            result[key] = answer["choice"]["label"]
+        elif answer["type"] == "choices":
+            result[key] = answer["choices"]["labels"]
+        else:
+            result[key] = None
+
+    return result
